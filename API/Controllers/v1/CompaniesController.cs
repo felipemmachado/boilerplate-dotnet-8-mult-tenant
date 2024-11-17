31d7b46376500806b6f55226cefaa9411e58d659
Felipe Machado <felipemmachado28@gmail.com>
2024-11-17 13:27:45 -0300

    chore: token

--- API/Controllers/v1/CompaniesController.cs
+++ API/Controllers/v1/CompaniesController.cs
@@ -1,14 +1,19 @@
 using API.Controllers.v1.@base;
+using Application.Common.Configs;
 using Application.Common.Exceptions;
 using Application.Queries.Companies.GetCompanyByUrl;
 using Application.UseCases.Companies.CreateCompany;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 namespace API.Controllers.v1
 {
     [AllowAnonymous]
-    public class CompaniesController : BaseController
+    public class CompaniesController(IOptions<ApiConfig> apiConfig) : BaseController
     {
+        private readonly ApiConfig _apiConfig = apiConfig.Value ?? throw new ArgumentNullException(nameof(apiConfig));
+
         [HttpGet("{url}/tenancy")]
         public async Task<GetCompanyDto> GetByUrl(string url)
         {
@@ -18,7 +23,7 @@ namespace API.Controllers.v1
         [HttpPost("{token}/sync")]
         public async Task<CreateCompanyDto> SyncCompany(string token, [FromBody] CreateCompanyCommand command)
         {
-            if (token == "joao1314")
+            if (token == _apiConfig.TokenCreateCompany)
             {
                 return await Mediator.Send(command);
             }
--- Application/Common/Configs/ApiConfig.cs
+++ Application/Common/Configs/ApiConfig.cs
@@ -3,5 +3,7 @@ namespace Application.Common.Configs
     public class ApiConfig
     {
         public string AppUrl { get; set; } = string.Empty;
+        
+        public string TokenCreateCompany { get; set; } = string.Empty;
     }
 }
